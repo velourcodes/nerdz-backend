3ee02f832553def238df59ee77ff257d15810df4
velourcodes <mohammadaffansiddiqi98@gmail.com>
2026-02-05 15:38:23 +0530

    FIxed report upload auth logic from non existent ADMIN to PATIENT

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -1,18 +1,17 @@
 import { Router } from "express";
 import { uploadReport } from "../controllers/report.controller.js";
-import {
-    JWTVerify,
-    authorizeRoles,
-} from "../middleware/auth.middleware.js";
+import { JWTVerify, authorizeRoles } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const reportRouter = Router();
 
-reportRouter.route("/upload").post(
-    JWTVerify,
-    authorizeRoles("DOCTOR"),
-    upload.single("report"),
-    uploadReport
-);
+reportRouter
+    .route("/upload")
+    .post(
+        JWTVerify,
+        authorizeRoles("PATIENT"),
+        upload.single("report"),
+        uploadReport
+    );
 
 export default reportRouter;
